35d5eae22772f91766119ddbb9767b09646bbfe4
Wellington <wellington.natsu1@gmail.com>
2021-05-02 16:31:54 -0300

    commitando primeiras partes do projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ErrorComponent } from './view/errors/error/error.component';
+import { HomeComponent } from './view/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
