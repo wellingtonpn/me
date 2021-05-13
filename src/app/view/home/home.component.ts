1ed635b2205b2a123dd3dcee165d1c7567b20ce9
Wellington <wellington.natsu1@gmail.com>
2021-05-13 19:46:17 -0300

    acertando a responsividade

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
 @Component({
@@ -7,14 +7,17 @@ import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
   styleUrls: ['./home.component.scss'],
   providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   imagemBack: string = '../../../assets/home/background.png';
   @ViewChild('modalAnimar') modalAnimar: HTMLElement;
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
+  
   ngOnInit(): void {
-    
+  }
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#008C8C';
   }
 
   animar(){
