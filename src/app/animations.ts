a17181a043373ca6b18ac834653d87aacae29f8d
Wellington <wellington.natsu1@gmail.com>
2021-05-16 17:06:49 -0300

    template responsivo

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -15,12 +15,12 @@ import {
       ),
       query(
         ':leave',
-        [style({ opacity: 1 }), animate('0.9s', style({ opacity: 0 }))],
+        [style({ opacity: 1 }), animate('0.4s', style({ opacity: 0 }))],
         { optional: true }
       ),
       query(
         ':enter',
-        [style({ opacity: 0 }), animate('0.9s', style({ opacity: 1 }))],
+        [style({ opacity: 0 }), animate('0.4s', style({ opacity: 1 }))],
         { optional: true }
       )
     ])
